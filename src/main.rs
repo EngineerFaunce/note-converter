af3fb63cfcdd7738bb04791765a0ca484e42aada
William Faunce <EngineerFaunce@users.noreply.github.com>
2023-05-01 03:08:11 +0000

    feat: add folder selection prompt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use crate::note::{noto::deserialize_noto_backup, keep::read_notes};
+use crate::note::{noto::{deserialize_noto_backup, prompt_folder_selection}, keep::read_notes};
 
 mod note;
 
 fn main() {
     let noto = deserialize_noto_backup();
+
+    let chosen_folder_id = prompt_folder_selection(&noto.folders);
 
     let source_notes = read_notes();
 }
