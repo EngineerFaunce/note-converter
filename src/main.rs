369b9ed0a1869c05a91def5810774116b6f57f27
William Faunce <EngineerFaunce@users.noreply.github.com>
2023-05-01 03:07:46 +0000

    feat: add google keep deserialization logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use crate::note::noto::deserialize_noto_backup;
+use crate::note::{noto::deserialize_noto_backup, keep::read_notes};
 
 mod note;
 
 fn main() {
     let noto = deserialize_noto_backup();
+
+    let source_notes = read_notes();
 }
--- src/note.rs
+++ src/note.rs
@@ -1,1 +1,2 @@
+pub mod keep;
 pub mod noto;
